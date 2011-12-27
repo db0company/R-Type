004c0d7405c58e055828631b2e4c0fca9c81a47e
Noelie <sylvain.noelie@gmail.com>
2011-12-27 21:00:28 +0100

    add clientnetwork.hpp. waiting for new architecture pour client :)

---
+++ src/client/ClientNetwork.hpp
@@ -0,0 +1,19 @@
+#ifndef  CLIENTNETWORK_H_
+# define CLIENTNETWORK_H_
+
+#include <iostream>
+#include <string>
+
+class		ClientNetwork
+{
+public:
+  ClientNetwork(void);
+  ~ClientNetwork(void);
+  ClientNetwork(const ClientNetwork&);
+  ClientNetwork&	operator=(const ClientNetwork&);
+
+private:
+
+};
+
+#endif	// CLIENTNETWORK_H_
