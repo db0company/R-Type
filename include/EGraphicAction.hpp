446f8ff5b96341f9bf7171742a9a7b560c6bd77e
Pardav <moutaw.i@gmail.com>
2012-01-05 23:30:44 +0100

    Avec la piece jointe c'est mieux TROLOLOLOOLOLOL

---
+++ include/EGraphicAction.hpp
@@ -0,0 +1,15 @@
+#ifndef EGRAPHICACTION_H
+# define EGRAPHICACTION_H
+
+enum eGraphicAction
+  {
+    START_PLAY,
+    ROOMLIST_REFRESH,
+    ROOMLIST_JOIN,
+    ROOMLIST_SPECTATE,
+    CREATE_CREATE,
+    ROOM_CHAT,
+    UNKNOWN
+  };
+
+#endif //EGRAPHICACTION_H
