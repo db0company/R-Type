e2ea8e3d5c0d92617f26c1502a9f58db3e256b6a
munoz_v <vincent.munoz91@gmail.com>
2012-01-09 04:14:41 +0100

    TimerWindows que j'avais oublié.

---
+++ src/server/TimerWindows.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "TimerWindows.hpp"
+
+TimerWindows::TimerWindows()
+{
+  this->start_time = 0;
+  this->time_to_wait = 0;
+}
+
+TimerWindows::~TimerWindows()
+{
+
+}
+
+bool TimerWindows::isTimeOut()const
+{
+  if (this->start_time == 0 || this->time_to_wait == 0)
+    {
+      std::cerr << "ERROR GETTIMEOFDAY" << std::endl;
+      return (true);
+    }
+  else if ((this->start_time + this->time_to_wait) < getTime())
+    return (true);
+ return (false);
+}
+
+DWORD	TimerWindows::getTime()const
+{
+  DWORD tmp;
+  
+  if ((tmp = GetTickCount())  == -1)
+    std::cerr << "ERROR GETTIMEOFDAY" << std::endl;
+  return (tmp);
+}
+  
+void	TimerWindows::initWait(unsigned int sec, unsigned int milli)
+{
+  DWORD tmp;
+
+  tmp = getTime();
+  this->time_to_wait = sec * 1000 + milli;
+}
+
+void TimerWindows::resetTime()
+{
+  this->start_time = getTime();
+}
